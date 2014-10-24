536d1e219dd4655cd7ac75c2ee86d64b0c2021a4
Abhijit <singh.abhijitpratap@gmail.com>
2014-10-24 15:08:44 +0530

    working for 3 result sets..testing remains to be done for large n result sets

--- src/result_collector.erl
+++ src/result_collector.erl
@@ -5,6 +5,7 @@ spawn_concurrent_append(Sql) ->
 	receive
 		{From, {data, Ref, Rows, Uid}} ->
 			io:format("~p\n",[From]),
+
 			Result = par_append_tuple:append_result_set(Ref, Rows, Uid),
 			case Result of
 				{result, ResultBody}  ->
